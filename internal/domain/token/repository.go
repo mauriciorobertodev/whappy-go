310e9ef76cc6ae1f8febd7a0e076dc5ab5bc4a76
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2025-10-08 18:53:51 -0300

    feat(token): add TokenQueryOptions and Count method to TokenRepository interface

--- internal/domain/token/repository.go
+++ internal/domain/token/repository.go
@@ -1,8 +1,38 @@
 package token
 
+type TokenQueryOptions struct {
+	ID         *string `db:"id"`
+	InstanceID *string `db:"instance_id"`
+
+	Limit   *int   `db:"limit"`
+	OrderBy string `db:"order_by"`
+	SortBy  string `db:"sort_by"`
+}
+
+type TokenQueryOption func(*TokenQueryOptions)
+
 type TokenRepository interface {
 	FindByID(id string) (*Token, error)
 	FindByInstanceID(instanceID string) ([]*Token, error)
 	Insert(token *Token) error
 	Delete(id string) error
+	Count(opts ...TokenQueryOption) int
+}
+
+func WhereID(id string) TokenQueryOption {
+	return func(o *TokenQueryOptions) {
+		o.ID = &id
+	}
+}
+
+func WhereInstanceID(instanceID string) TokenQueryOption {
+	return func(o *TokenQueryOptions) {
+		o.InstanceID = &instanceID
+	}
+}
+
+func Limit(limit int) TokenQueryOption {
+	return func(o *TokenQueryOptions) {
+		o.Limit = &limit
+	}
 }
